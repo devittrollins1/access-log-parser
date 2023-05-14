1ec75e95e93062932ba034e101b1f1e628ac457a
Vitaly Tabakov <v.tabakov321@gmail.com>
2023-05-15 01:56:09 +0300

    New program added to main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int addition = firstNumber + secondNumber;
+        System.out.println(addition);
+        int subtraction = firstNumber - secondNumber;
+        System.out.println(subtraction);
+        int multiplication = firstNumber * secondNumber;
+        System.out.println(multiplication);
+        double division = (double) firstNumber / secondNumber;
+        System.out.println(division);
+
     }
 
 }
